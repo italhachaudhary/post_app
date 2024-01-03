375fdb147b8c307b512667c3f05b7e5ceb3222ea
Talha <admin@talha.com>
2024-01-03 10:54:30 +0500

    displayimg.jsx

--- src/App.js
+++ src/App.js
@@ -1,18 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          <h1>React App</h1>
-        </p>
-      </header>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
---
+++ src/components/displayImg.jsx
@@ -0,0 +1,10 @@
+//
+
+import React from "react";
+
+export default function DisplayImg() {
+  return;
+  <div>
+    <img src="https://picsum.photos/200/300" alt="random" />
+  </div>;
+}
