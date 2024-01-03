1565eae3da8a6ce8cbab04b5c7a7d9adc7d5d93c
Talha <admin@talha.com>
2024-01-03 10:33:05 +0500

    H1

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <h1>React App</h1>
         </p>
       </header>
     </div>
