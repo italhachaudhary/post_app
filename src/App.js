a4d42496babe5d81dac430e15298f790ffdad1bc
Talha <admin@talha.com>
2024-01-22 10:44:39 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import { Routes, Route } from "react-router";
-
 import NavBar from "./components/NavBar";
 import Details from "./components/Details";
 import { BrowserRouter } from "react-router-dom";
@@ -9,13 +8,12 @@ import Banner from "./components/Banner";
 function App() {
   return (
     <BrowserRouter>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Banner />} />
-          <Route path="/details/:id" element={<Details />} />
-        </Routes>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Banner />} />
+        <Route path="/details/:id" element={<Details />} />
+      </Routes>
     </BrowserRouter>
   );
 }
-
 export default App;
