263cb06a6a67cf7958ef15dc4079642a2c443a5c
Talha <admin@talha.com>
2024-01-25 10:55:45 +0500

    commit 25-01-2024

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,18 +1,22 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { data } from "../components/PostData";
 
 export default function Details() {
-  const { state } = useLocation();
+  const { id } = useParams();
+  const post = data.find((post) => post.id == id);
 
   return (
-    <div className="container">
-        <div className="card mb-3" key={state.id}>
+    <div>
+      <div className="container">
+        <div className="card mb-3" key={post.id}>
           <div className="card-body">
-            <h3 className="card-title">{state.title}</h3>
-          <p className="card-text text-black">{state.body}</p>
-          <p className="card-text text-black">{state.id}</p>
+            <h3 className="card-title">{post.title}</h3>
+            <p className="card-text text-black">{post.body}</p>
+            <p className="card-text text-black">{post.id}</p>
           </div>
         </div>
-  </div>
+      </div>
+    </div>
   );
 }
