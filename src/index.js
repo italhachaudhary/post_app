79edef074ffa7d9fe4614ba4804ea602496de95d
Talha <admin@talha.com>
2024-02-06 11:31:08 +0500

    MUI

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import "@mui/material/";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
